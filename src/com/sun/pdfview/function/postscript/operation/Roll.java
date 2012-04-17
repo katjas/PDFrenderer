c128c5f4f689b8dae501117b66673cba23ee7dd8
Boris von Loesch <boris@vonloesch.de>
2012-04-17 09:06:05 +0200

    Implement two previously unimplemented postscript functions

--- src/com/sun/pdfview/function/postscript/operation/Roll.java
+++ src/com/sun/pdfview/function/postscript/operation/Roll.java
@@ -7,8 +7,15 @@ final class Roll implements PostScriptOperation {
 	public void eval(Stack<Object> environment) {
 	    // <i>anyn-1 ... any0 n j</i> <b>roll</b> <i>any(j-1)mod n ... anyn-1 ... any</i>
 	    // Roll n elements up j times
-	    Object obj = environment.pop();
-	    environment.push(obj);
-	    environment.push(obj);
+		long j = Math.round((Double)environment.pop());
+		long n = Math.round((Double)environment.pop());
+		Object[] topN = new Object[(int)n];
+		for (int i=0; i < n; i++) topN[i] = environment.pop();
+		for (int i=0; i < n; i++) {
+			int k = (int)(i - j);
+			if (k < 0) k += n;
+			if (k >= n) k-= n;
+			environment.push(topN[k]);
+		}
 	}
 }
