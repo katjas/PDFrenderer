dee0380e7d4c470f1858a9a63c77b8e8d367d8fe
Boris von Loesch <boris@vonloesch.de>
2012-04-18 14:00:08 +0200

    Fix 'roll' implementation. Add some simple JUnit test for the postscript operators.

--- src/com/sun/pdfview/function/postscript/operation/Roll.java
+++ src/com/sun/pdfview/function/postscript/operation/Roll.java
@@ -7,14 +7,14 @@ final class Roll implements PostScriptOperation {
 	public void eval(Stack<Object> environment) {
 	    // <i>anyn-1 ... any0 n j</i> <b>roll</b> <i>any(j-1)mod n ... anyn-1 ... any</i>
 	    // Roll n elements up j times
-		long j = Math.round((Double)environment.pop());
-		long n = Math.round((Double)environment.pop());
+		int j = ((Number)environment.pop()).intValue();
+		int n = ((Number)environment.pop()).intValue();
 		Object[] topN = new Object[(int)n];
 		for (int i=0; i < n; i++) topN[i] = environment.pop();
+		
 		for (int i=0; i < n; i++) {
-			int k = (int)(i - j);
+			int k = (-i + j - 1) % n;
 			if (k < 0) k += n;
-			if (k >= n) k-= n;
 			environment.push(topN[k]);
 		}
 	}
