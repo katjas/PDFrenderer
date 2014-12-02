b827c61be293e74828a0ab0cbc9cb0ab1b2aea22
katjas <k.sondermann@gmx.net>
2014-12-02 15:57:28 +0100

    Remove printing to console

--- src/com/sun/pdfview/font/PDFGlyph.java
+++ src/com/sun/pdfview/font/PDFGlyph.java
@@ -17,77 +17,85 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 package com.sun.pdfview.font;
+
 import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 
 import com.sun.pdfview.PDFPage;
 import com.sun.pdfview.PDFShapeCmd;
+
 /**
 * A single glyph in a stream of PDF text, which knows how to write itself
 * onto a PDF command stream
 */
 public class PDFGlyph {
-/** the character code of this glyph */
-private final char src;
-/** the name of this glyph */
-private final String name;
-/** the advance from this glyph */
-private final Point2D advance;
-/** the shape represented by this glyph (for all fonts but type 3) */
-private GeneralPath shape;
-/** the PDFPage storing this glyph's commands (for type 3 fonts) */
-private PDFPage page;
-/** Creates a new instance of PDFGlyph based on a shape */
-public PDFGlyph(char src, String name, GeneralPath shape,
-Point2D.Float advance) {
-this.shape = shape;
-this.advance = advance;
-this.src = src;
-this.name = name;
-}
-/** Creates a new instance of PDFGlyph based on a page */
-public PDFGlyph(char src, String name, PDFPage page, Point2D advance) {
-this.page = page;
-this.advance = advance;
-this.src = src;
-this.name = name;
-}
-/** Get the character code of this glyph */
-public char getChar() {
-return this.src;
-}
-/** Get the name of this glyph */
-public String getName() {
-return this.name;
-}
-/** Get the shape of this glyph */
-public GeneralPath getShape() {
-return this.shape;
-}
-/** Get the PDFPage for a type3 font glyph */
-public PDFPage getPage() {
-return this.page;
-}
-/** Add commands for this glyph to a page */
-public Point2D addCommands(PDFPage cmds, AffineTransform transform, int mode) {
-if (this.shape != null) {
-System.out.println("[text] shape: " + src);
-GeneralPath outline = (GeneralPath) this.shape.createTransformedShape(transform);
-cmds.addCommand(new PDFShapeCmd(outline, mode));
-} else if (this.page != null) {
-System.out.println("[text] page: " + src);
-cmds.addCommands(this.page, transform);
-}
-return this.advance;
-}
-public Point2D getAdvance() {
-return advance;
-}
-@Override
-public String toString() {
-StringBuffer str = new StringBuffer();
-str.append(this.name);
-return str.toString();
-}
+    /** the character code of this glyph */
+    private final char src;
+    /** the name of this glyph */
+    private final String name;
+    /** the advance from this glyph */
+    private final Point2D advance;
+    /** the shape represented by this glyph (for all fonts but type 3) */
+    private GeneralPath shape;
+    /** the PDFPage storing this glyph's commands (for type 3 fonts) */
+    private PDFPage page;
+
+    /** Creates a new instance of PDFGlyph based on a shape */
+    public PDFGlyph(char src, String name, GeneralPath shape, Point2D.Float advance) {
+        this.shape = shape;
+        this.advance = advance;
+        this.src = src;
+        this.name = name;
+    }
+
+    /** Creates a new instance of PDFGlyph based on a page */
+    public PDFGlyph(char src, String name, PDFPage page, Point2D advance) {
+        this.page = page;
+        this.advance = advance;
+        this.src = src;
+        this.name = name;
+    }
+
+    /** Get the character code of this glyph */
+    public char getChar() {
+        return this.src;
+    }
+
+    /** Get the name of this glyph */
+    public String getName() {
+        return this.name;
+    }
+
+    /** Get the shape of this glyph */
+    public GeneralPath getShape() {
+        return this.shape;
+    }
+
+    /** Get the PDFPage for a type3 font glyph */
+    public PDFPage getPage() {
+        return this.page;
+    }
+
+    /** Add commands for this glyph to a page */
+    public Point2D addCommands(PDFPage cmds, AffineTransform transform, int mode) {
+        if (this.shape != null) {
+            GeneralPath outline = (GeneralPath) this.shape.createTransformedShape(transform);
+            cmds.addCommand(new PDFShapeCmd(outline, mode));
+        } else if (this.page != null) {
+            cmds.addCommands(this.page, transform);
+        }
+        return this.advance;
+    }
+
+    public Point2D getAdvance() {
+        return advance;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer str = new StringBuffer();
+        str.append(this.name);
+        return str.toString();
+    }
 }
