505b4aa579c53880242f1c39f987bbb32e13df2e
katjas <k.sondermann@gmx.net>
2014-12-09 09:58:37 +0100

    Remove writing to text file

--- src/com/sun/pdfview/PDFParser.java
+++ src/com/sun/pdfview/PDFParser.java
@@ -24,7 +24,6 @@ import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -136,15 +135,6 @@ public class PDFParser extends BaseWatchable {
             this.resources = new HashMap<String, PDFObject>();
         }
         this.stream = stream;
-        try {
-            FileOutputStream fos = new FileOutputStream(new File("output.txt"));
-            fos.write(stream);
-            fos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     // ///////////////////////////////////////////////////////////////
